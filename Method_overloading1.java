d111169dfb91e28ab31cbceefe33b0e352fecb1c
R. megha shyam <meghashyam997@gmail.com>
2026-03-31 19:18:40 +0530

    added some lines of code in both java files

--- Method_overloading1.java
+++ Method_overloading1.java
@@ -1,69 +1,79 @@
 public class Method_overloading1 {
     /*1.Create a program for Student Information using method overloading.*/
-    public void student_info(String name,int age,String course){
-    
-        System.out.println("my name is: "+name);
-        System.out.println("my age is: "+age);
-        System.out.println("my course is: "+course);
-    }
+   public void student()
+{
+    System.out.println("Student details");
+}
+
+public void student(String name)
+{
+    System.out.println("Name: " + name);
+}
+
+public void student(String name, int age, String course)
+{
+    System.out.println("Name: " + name);
+    System.out.println("Age: " + age);
+    System.out.println("Course: " + course);
+}
     /*2.Create a program for Addition Calculator using method overloading.*/
-    public void addition(int a,int b){
+    public void addition(int a, int b){
         int sum=a+b;
         System.out.println("The sum of two numbers are: "+sum);
     }
     /*3.Create a program for Employee Salary System using method overloading.*/
-    public void employee_salary_system(int basic_salary,int bonous,int allowance){
-        System.out.println("Employee basic salary is: "+basic_salary+" and bonous salary is: "+bonous+" and allowance is: "+allowance);
+    public void salary(int basic, int bonus, int allowance){
+        System.out.println("Employee basic salary is: "+basic+" and bonous salary is: "+bonus+" and allowance is: "+allowance);
     }
     /*4.Create a program for Car Showroom Details using method overloading.*/
-    public void car_showroom(String car_name,int price,String color,String model,String company){
-        System.out.println(" The car name is: "+car_name+" \n car price is: "+price+" \n car color is: "+color+ "\n car model is: "+model+" \n company name is: "+company);
+    public void car(String name, int price, String color, String model){
+        System.out.println(" The car name is: "+name+" \n car price is: "+price+" \n car color is: "+color+ "\n car model is: "+model);
     }
     /*5.Create a program for Mobile Store Details using method overloading.*/
-    public void mobile_details(String mobile_name, int price, String model){
-        System.out.println(" The mobile name is: "+mobile_name+"\n  mobile price is: "+price+"\n  mobile model is: "+model);
+    public void mobile(String name, int price, String model){
+        System.out.println(" The mobile name is: "+name+"\n  mobile price is: "+price+"\n  mobile model is: "+model);
     }
     /*6.Create a program for Area Calculator (different shapes) using method overloading.*/
-    public int area_calculator(int length,int breath){
+    public int area(int length, int breath){
     int area=length*breath;
     return area;
     }
     /*7.Create a program for Bank Transaction System using method overloading.*/
-    public void bank_transcation(String account_name,String account_number,int amount){
-        System.out.println("Account name is: "+account_name+"\n account_number is"+account_number+"\n amount is: "+amount);
+    public void transaction(String account, int amount){
+        System.out.println("Account name is: "+account+"\n amount is: "+amount);
     }
     /*8.Create a program for Course Details using method overloading.*/
-    public void course_details(String course_name, int course_duration,int course_fee){
-        System.out.println("I am pursuing this course "+course_name+" for "+course_duration+"months and course fee is: "+course_fee);
+    public void course(String name, int duration, int fee){
+        System.out.println("I am pursuing this course "+name+" for "+duration+"months and course fee is: "+fee);
     }
     /*9.Create a program for Book Information System using method overloading.*/
-    public String book_details(String book_name,String author_name,int price){
-        return "The bbok name is: "+book_name+" and author name is: "+author_name+" and price is: "+price;
+    public String book(String name, String author, int price){
+        return "The bbok name is: "+name+" and author name is: "+author+" and price is: "+price;
     }
     /*10.Create a program for Hospital Management System using method overloading.*/
-    public String hospital_details(int patient_count,int age,int carona_virus_count){
-        return "The patient count is: "+patient_count+" and age is: "+age+" and coronavirus count is: "+carona_virus_count;
+    public String hospital(String name, int patients, int doctors){
+        return "The hospital name is: "+name+" and patient count is: "+patients+" and doctor count is: "+doctors;
     }
     public static void main(String[] args) {
         Method_overloading1 student=new Method_overloading1();
-        student.student_info("R.Megha Shyam", 22, "Full Stack Java Developer");
+        student.student("R.Megha Shyam", 22, "Full Stack Java Developer");
         Method_overloading1 adding=new Method_overloading1();
         adding.addition(10, 20);
         Method_overloading1 employee=new Method_overloading1();
-        employee.employee_salary_system(1000000,2000000 , 30000);
+        employee.salary(1000000,2000000 , 30000);
         Method_overloading1 car=new Method_overloading1();
-        car.car_showroom("Toyota",500000,"black","2025","Toyota motors");
+        car.car("Toyota",500000,"black","2025");
         Method_overloading1 mobile=new Method_overloading1();
-        mobile.mobile_details("samsung", 200000, "galaxy 42l");
+        mobile.mobile("samsung", 200000, "galaxy 42l");
         Method_overloading1 area1=new Method_overloading1();
-        System.out.println("The area of rectangle is:"+area1.area_calculator(5, 10));
+        System.out.println("The area of rectangle is:"+area1.area(5, 10));
         Method_overloading1 bank=new Method_overloading1();
-        bank.bank_transcation("R.megha shyam","sbi",1100000);
+        bank.transaction("R.megha shyam",1100000);
         Method_overloading1 course=new Method_overloading1();
-        course.course_details("Full stack java",6,24000);
+        course.course("Full stack java",6,24000);
         Method_overloading1 book=new Method_overloading1();
-        System.out.println(book.book_details("vajrayudam", "R.Megha Shyam",200000));
+        System.out.println(book.book("vajrayudam", "R.Megha Shyam",200000));
         Method_overloading1 hospital=new Method_overloading1();
-        System.out.println(hospital.hospital_details(20000, 290728974, 737397973));
+        System.out.println(hospital.hospital("Apollo Hospitals", 20000, 290728974));
         }
 }
--- Method_overloading2.java
+++ Method_overloading2.java
@@ -13,6 +13,41 @@ public class Method_overloading2 {
     public int bike_details(int price){
         return  price;
     }
+    /*14.Create a program for City Information System using method overloading*/
+    public String city_information(String city_name,int population){
+        return "The city name is: "+city_name+" and the city population is: "+population;
+    }
+    /*15.Create a program for Game Details System using method overloading*/
+    public void game_system(){
+        System.out.println("The game name is :free_fire");
+        System.out.println("Number of players are: multi-player");
+        System.out.println("The game price is: free");
+    }
+    /*16.Create a program for Library Management System using method overloading.*/
+    public String library_details(String book_name,String author_name,int nuMber_of_books){
+        return "The book name is: "+book_name+" and the book author is: "+author_name+" and number of books are: "+nuMber_of_books;
+    }
+    /*17.Create a program for Restaurant Information System using method overloading.*/
+    public void restaurant_system(){
+        System.out.println("The restaurant name is: pai vaisory");
+        System.err.println("The restaurant location is: Tirupati");
+        System.out.println("The restaurant rating is: 5.5/10");
+        System.out.println("The restaurant quantity is super");
+    }
+    /*18.Create a program for School Management System using method overloading.*/
+    public int school_details(int number_of_boys,int number_of_girls){
+        return number_of_boys+number_of_girls;
+    }
+    /*19.Create a program for Technology Company System using method overloading.*/
+    public void technology_company(){
+        System.out.println("The company name is: sri vency company");
+        System.out.println("The company location is: Tirupati");
+        System.out.println("The company CEO IS: Bhanu prakash");
+    }
+    /*20.Create a program for Vehicle Information System using method overloading*/
+    public String vehicle_details(String vehicle_name,String vehicle_model,String vehicle_type,int vehicle_price){
+        return "The vehicle name is: "+vehicle_name+"\n The vehicle model is: "+vehicle_type+"\n The vehicle price is"+vehicle_price;
+    }
     public static void main(String[] args) {
         Method_overloading2 product=new Method_overloading2();
         product.product_details();
@@ -20,5 +55,19 @@ public class Method_overloading2 {
         System.out.println(movie.movie_details());
         Method_overloading2 bike=new Method_overloading2();
         System.out.println("bike price is: "+bike.bike_details(20000));  
+        Method_overloading2 city=new Method_overloading2();
+        System.out.println(city.city_information("Tirupati",500000000));
+        Method_overloading2 game=new Method_overloading2();
+        game.game_system();
+        Method_overloading2 library=new Method_overloading2();
+        System.out.println(library.library_details("vajrayudam", "R.Megha Shyam", 100));
+        Method_overloading2 restaurant=new Method_overloading2();
+        restaurant.restaurant_system();
+        Method_overloading2 school=new Method_overloading2();
+        System.out.println("The total number of students are: "+school.school_details(25,45));
+        Method_overloading2 company=new Method_overloading2();
+        company.technology_company();
+        Method_overloading2 vehicle=new Method_overloading2();
+        System.out.println(vehicle.vehicle_details("toyota", "camry", "sedan", 200000));
      }
 }
